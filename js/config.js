c079bf77d9bc5b0958f26e83f5510ff37bd2fdb9
Aadi639707 <aadi639707@gmail.com>
2026-07-04 12:51:29 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // Firebase Configuration (v10 Modular)
 export const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
+    apiKey: "AIzaSyBEUHICLM_3ynHWwnrvVSZuGd34ti590lk",
     authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
+    projectId: "recoverpro-7591d",
     storageBucket: "YOUR_PROJECT.appspot.com",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
