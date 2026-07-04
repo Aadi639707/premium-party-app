779d4955bb28c2017b843912681185e4b38fa075
Aadi639707 <aadi639707@gmail.com>
2026-07-04 14:30:09 +0530

    Update Firebase configuration for recoverpro-7591d

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 // Firebase Configuration (v10 Modular)
 export const firebaseConfig = {
     apiKey: "AIzaSyBEUHICLM_3ynHWwnrvVSZuGd34ti590lk",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    authDomain: "recoverpro-7591d.firebaseapp.com",
     projectId: "recoverpro-7591d",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    storageBucket: "recoverpro-7591d.firebasestorage.app",
+    messagingSenderId: "793303842896",
+    appId: "1:793303842896:web:c5c0dfc443cd3ec11f9155"
 };
 
 // Agora Real-Time Voice Configuration
-export const AGORA_APP_ID = "092778fb8294423abad0d939911b9ef4"; // Get from agora.io console
+export const AGORA_APP_ID = "092778fb8294423abad0d939911b9ef4"; 
